defaf6be92c5f24dbe3bf9b05b60e4d9becf88d7
Benjamin Geißler <b.geissler89@gmail.com>
2021-07-28 19:10:05 +0200

    init

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+brew install --cask adobe-acrobat-reader
+brew install --cask adobe-creative-cloud
+# burn images to a usb drive
+brew install --cask balenaetcher
+# Control multiple Mac with just one Keyboard / Mouse
+brew install --cask barrier
+brew install --cask burp-suite
+brew install --cask cheatsheet
+
+brew install --cask docker
+brew install --cask figma
+# browser which u like
+brew install --cask google-chrome
+#brew install --cask chromium
+
+# Kubernetes UI
+brew install --cask lens maccy postman obs iconset insomnia
+
+# Need Windows RDP?
+# brew install --cask microsoft-remote-desktop
+
+brew install --cask owasp-zap
+
+# Create Animations
+brew install --cask principle
+
+# Messenger
+brew install --cask telegram
+
+# The one IDE you ever need
+brew install --cask visual-studio-code
+
+brew install --cask vlc
+brew install ansible
+brew install asciinema
+brew install azure-cli
+brew install cntlm
+brew install docker
+brew install docker-compose 
+brew install git
+# Kubernetes Package Manager
+brew install helm
+brew install iterm2 
+brew install kubernetes-cli 
+brew install zsh 
+brew install node@14 #LTS
+brew install taskwarrior-tui
+brew install terraform
+# time tracking on cli
+brew install timewarrior
+brew install tree 
+brew install zsh
+brew install htop
+brew install glances
