03f0d705a220f54ad591e3cf4e6a213c07947bb5
Benjamin Geißler <b.geissler89@gmail.com>
2021-08-13 08:37:09 +0200

    install brew before and reordering

--- install.sh
+++ install.sh
@@ -1,56 +1,77 @@
 #!/bin/sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# The one IDE you ever need
+brew install --cask visual-studio-code
+brew install git
+
+# Commons
+brew install --cask vlc
 brew install --cask adobe-acrobat-reader
+brew install iterm2 
+brew install tree 
+brew install zsh
+brew install htop
+brew install glances
+
+
+# For Adobe XD f.e.
 brew install --cask adobe-creative-cloud
+brew install --cask figma
+brew install iconset
+# Create Animations
+brew install --cask principle
+
 # burn images to a usb drive
-brew install --cask balenaetcher
+#brew install --cask balenaetcher
+
 # Control multiple Mac with just one Keyboard / Mouse
 brew install --cask barrier
-brew install --cask burp-suite
-brew install --cask cheatsheet
 
+# HTTP Interception and Security Forensic
+brew install --cask burp-suite
+brew install --cask owasp-zap
+brew install postman maccy obs 
+brew install insomnia
+
+brew install docker
+brew install docker-compose 
 brew install --cask docker
-brew install --cask figma
+
 # browser which u like
 brew install --cask google-chrome
 #brew install --cask chromium
 
 # Kubernetes UI
-brew install --cask lens maccy postman obs iconset insomnia
+brew install helm
+brew install --cask lens 
+brew install kubernetes-cli 
+brew install ansible
+brew install terraform
+
+# Cloud
+brew install azure-cli
+
+# Local Proxy
+brew install cntlm
 
 # Need Windows RDP?
 # brew install --cask microsoft-remote-desktop
 
-brew install --cask owasp-zap
-
-# Create Animations
-brew install --cask principle
-
 # Messenger
 brew install --cask telegram
 
-# The one IDE you ever need
-brew install --cask visual-studio-code
-
-brew install --cask vlc
-brew install ansible
+# Take recordings of shell sessions
 brew install asciinema
-brew install azure-cli
-brew install cntlm
-brew install docker
-brew install docker-compose 
-brew install git
-# Kubernetes Package Manager
-brew install helm
-brew install iterm2 
-brew install kubernetes-cli 
-brew install zsh 
+
+# Another Shell
+brew install zsh
+
+#Nodejs
 brew install node@14 #LTS
+
+# TODO List on CLI
 brew install taskwarrior-tui
-brew install terraform
 # time tracking on cli
 brew install timewarrior
-brew install tree 
-brew install zsh
-brew install htop
-brew install glances
